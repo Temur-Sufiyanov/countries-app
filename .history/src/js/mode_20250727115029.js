7e2de16889c12ad26b182ac800efa92c61237d68
Temur-Sufiyanov <temursufiyanov@gmail.com>
2025-07-27 12:05:02 +0900

    Countries app

---
+++ .history/src/js/mode_20250727115029.js
@@ -0,0 +1,22 @@
+const modeBtn = document.querySelector('.header__dark-mode')
+const body = document.querySelector("body")
+const modeFromLocal = localStorage.getItem('mode') ? localStorage.getItem('mode') : null
+
+if (modeFromLocal) {
+    body.classList.add('dark-mode')
+}
+
+modeBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-mode')
+    modeFromLocal ? localStorage.setItem('mode', '') : localStorage.setItem('mode', 'dark') 
+    
+    if (isDark) {
+        document.body.classList.remove("light");
+        themeIcon.textContent = "🌞"; // Sun icon
+        themeLabel.textContent = "Light Mode";
+    } else {
+        document.body.classList.add("light");
+        themeIcon.textContent = "🌙"; // Moon icon
+        themeLabel.textContent = "Dark Mode";
+    }
+})
---
+++ .history/src/js/mode_20250727115522.js
@@ -0,0 +1,22 @@
+const modeBtn = document.querySelector('.header__dark-mode')
+const modeSpan = document.querySelector('.header__dark-mode span')
+const body = document.querySelector("body")
+const modeFromLocal = localStorage.getItem('mode') ? localStorage.getItem('mode') : null
+
+if (modeFromLocal) {
+    body.classList.add('dark-mode')
+    modeSpan.textContent = "Lig Mode";
+}
+
+modeBtn.addEventListener('click', () => {
+      const isDark = body.classList.toggle('dark-mode')
+    modeFromLocal ? localStorage.setItem('mode', '') : localStorage.setItem('mode', 'dark') 
+    
+    if (isDark) {
+        document.body.classList.remove("light");
+        modeSpan.textContent = "Light Mode";
+    } else {
+        document.body.classList.add("light");
+        modeSpan.textContent = "Dark Mode";
+    }
+})
